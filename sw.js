81742b0f10d1195ee6cf1a348d82d1de3eb5ba2d
Burrows87 <cristianlollo@hotmail.it>
2026-03-22 09:19:04 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE_NAME = 'looply-v1';
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(['/', '/index.html', '/style.css', '/app.js', '/manifest.json']))
-  );
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
